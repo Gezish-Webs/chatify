ba34dfdb25c7c21eea00637fb015e0886cb9961a
Gezish-Webs <gezahegnaberamegenasa@gmail.com>
2025-11-06 19:58:13 +0300

    ch
    chhange1

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -1,9 +1,29 @@
-import { Resend } from "resend";
-import { ENV } from "./env.js";
+// import { Resend } from "resend";
+// import { ENV } from "./env.js";
 
-export const resendClient = new Resend(ENV.RESEND_API_KEY);
+// export const resendClient = new Resend(ENV.RESEND_API_KEY);
+
+// export const sender = {
+//   email: ENV.EMAIL_FROM,
+//   name: ENV.EMAIL_FROM_NAME,
+// };
+
+
+// import { Resend } from "resend";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// export default resend;
+
+import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config(); // load variables from .env file
+
+export const resendClient = new Resend(process.env.RESEND_API_KEY);
 
 export const sender = {
-  email: ENV.EMAIL_FROM,
-  name: ENV.EMAIL_FROM_NAME,
+  email: process.env.EMAIL_FROM,
+  name: process.env.EMAIL_FROM_NAME,
 };
